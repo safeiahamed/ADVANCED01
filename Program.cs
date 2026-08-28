b63ff4165c4d172525c6a26bd78054edc4994028
Safeia90 <safeiahamed@fci.bu.edu.eg>
2026-08-28 19:08:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentAdv_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
